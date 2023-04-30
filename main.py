6d6bb8bde07a1e82ce7e68a85cfffc29a715d5c3
EdTheCHamp <e.ernests.mathers@gmail.com>
2023-04-30 13:50:56 +0300

    scrolling work wo/ button. will fix later. gotta go gym.

--- main.py
+++ main.py
@@ -6,28 +6,33 @@ root.title("General Progress Tracker")
 root.geometry("600x200")
 root.resizable(False,False)
 
-welcome = tk.Label(root,text="Welcome to the app \n select a trackable or create a new one!",font=("Arial",25))
-welcome.place(x=300,y=40,anchor="center")
+canvas=tk.Canvas(root,bg="white",scrollregion=(0,0,20000,50000))
+canvas.pack(expand= True, fill= 'both')
+
+welcome = tk.Label(canvas,text="Welcome to the app \n select a trackable or create a new one!",font=("Arial",25))
+canvas.create_window(300,40,window=welcome)
+
+canvas.bind('<MouseWheel>',lambda event:canvas.yview_scroll(-int(event.delta/60),"units"))
 
 
 def frame_make():
-    #resize window to fit
     global trackable_count
 
     # create frame
-    frame = tk.Frame(root, width=400, height=150, bg="blue")
-    frame.place(x=300, anchor="center", y=180 + trackable_count*180)
+    frame = tk.Frame(canvas, width=400, height=150, bg="blue")
+    canvas.create_window(300,180+trackable_count*180,window=frame)
 
     trackable_count +=1
-    root.geometry(f"600x{200+170*trackable_count}")
+    if trackable_count <=4:
+        # resize window to fit
+        root.geometry(f"600x{200+170*trackable_count}")
 
-    #replace btn create_new
-    create_new.place_configure(x=450,anchor="center",y=180+170*trackable_count)
+    btn_height= 180+170*trackable_count
+    print(btn_height)
+    canvas.moveto("create_new",450,btn_height)
 
 
-
-create_new = tk.Button(root,text="create new trackable",command=frame_make)
-create_new.place(x=450,y=180,anchor="center")
-
+create_new = tk.Button(canvas,text="create new trackable",command=frame_make)
+canvas.create_window(450,180,window=create_new)
 # Run the window
 root.mainloop()
